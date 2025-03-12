702939b11fc76390e15edbff47f93e6022b75a10
unknown <kiperdemiwan@gmail.com>
2025-03-12 20:59:05 +0200

    result

--- src/routes/profiles.py
+++ src/routes/profiles.py
@@ -1,5 +1,47 @@
 from fastapi import APIRouter
+from schemas import ProfileResponseSchema, ProfileCreate
+
+
+from src.database.models.accounts import UserProfileModel
 
 router = APIRouter()
 
-# Write your code here
+@router.post("/users/{user_id}/profile/", response=ProfileResponseSchema)
+async def create_profile(
+        profile_data: ProfileCreate,
+        db: AsyncSession = Depends(get_db),
+):
+    existing_stmt = select(UserProfileModel).where(
+        (UserProfileModel.name == profile_data.name),
+        (UserProfileModel.date == profile_data.date)
+    )
+    existing_result = await db.execute(existing_stmt)
+    existing_movie = existing_result.scalars().first()
+    if existing_movie:
+        raise HTTPException(
+            status_code=404,
+        )
+
+    if not profile_data.is_active:
+        raise HTTPException(
+            status_code=404,
+        )
+
+    if profile_data.is_active:
+        raise HTTPException(
+            status_code=404,
+        )
+
+    new_profile = UserProfileModel(
+        id=profile_data.id,
+        first_name=profile_data.first_name,
+        avatar=profile_data.avatar,
+        gender=profile_data.gender,
+        date_of_birth=profile_data.date_of_birth,
+        info=profile_data.info
+    )
+
+    db.add(new_profile)
+    db.commit(new_profile)
+    db.refresh(new_profile)
+    return new_profile
--- src/schemas/profiles.py
+++ src/schemas/profiles.py
@@ -10,4 +10,51 @@ from validation import (
     validate_birth_date
 )
 
-# Write your code here
+class ProfileCreate(BaseModel):
+    @field_validator("name")
+    @classmethod
+    def validate_name_field(cls, value):
+        return validate_name(value)
+
+    @field_validator("gender")
+    @classmethod
+    def validate_gender_field(cls, value):
+        return validate_gender(value)
+
+    @field_validator("date_of_birth")
+    @classmethod
+    def validate_birth_date_field(cls, value):
+        return validate_birth_date(value)
+
+    @field_validator("avatar")
+    @classmethod
+    def validate_avatar_field(cls, value):
+        return validate_image(value) if value else None
+
+
+class ProfileResponseSchema(BaseModel):
+    name: str = Field(..., description="User's name")
+    gender: str = Field(..., description="User's gender")
+    date_of_birth: date = Field(..., description="User's birth date")
+    info: str
+    avatar: UploadFile | None = None
+
+    @field_validator("name")
+    @classmethod
+    def validate_name_field(cls, value):
+        return validate_name(value)
+
+    @field_validator("gender")
+    @classmethod
+    def validate_gender_field(cls, value):
+        return validate_gender(value)
+
+    @field_validator("date_of_birth")
+    @classmethod
+    def validate_birth_date_field(cls, value):
+        return validate_birth_date(value)
+
+    @field_validator("avatar")
+    @classmethod
+    def validate_avatar_field(cls, value):
+        return validate_image(value) if value else None
